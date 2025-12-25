c137d0210e0400093fc0b0d708cc1a92e3f3f269
AsakaKudo <ptpradnya2@gmail.com>
2025-12-26 02:27:55 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const commands = require('./commands');
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Asa siap bertugas.');
+});
+
+client.on('message', msg => {
+    const text = msg.body.toLowerCase().trim();
+    if (commands[text]) {
+        commands[text](msg);
+    }
+});
+
+client.initialize();
