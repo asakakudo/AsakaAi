f3dbc9c91e41d5b2289d4ad74b34e493148bc86f
AsakaKudo <ptpradnya2@gmail.com>
2025-12-27 17:49:07 +0800

    Delete commands.js

--- commands.js
+++
@@ -1,7 +0,0 @@
-const commands = {
-    halo: msg => msg.reply('halo juga'),
-    waktu: msg => msg.reply(new Date().toLocaleString()),
-    ulang: msg => msg.reply(msg.body)
-};
-
-module.exports = commands;
